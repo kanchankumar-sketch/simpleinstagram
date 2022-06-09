2293566189daa578f380489c4e11d878966fbbcc
Kanchan kumar Sharma <70506833+kanchankumar-sketch@users.noreply.github.com>
2022-06-09 07:16:04 +0530

    added code of instagram

--- src/main/java/com/connecting/repositories/UserPostsRepository.java
+++ src/main/java/com/connecting/repositories/UserPostsRepository.java
@@ -11,9 +11,9 @@ import com.connecting.model.UserPosts;
 @Repository
 public interface UserPostsRepository extends JpaRepository<UserPosts,Long> {
 
-	@Query(value = "SELECT pr.user_id as id,pr.profile_name as profileName,pr.profile_image_path as profilePath,po.image_post_path as postFilePath,po.description,po.location,po.id as idea FROM `user_profile` as pr,`user_posts` as po where po.user_id=pr.user_id and pr.user_id IN (SELECT x.follwed_user_id from user_follow as x where x.user_id=:id) order by po.id desc limit 5",nativeQuery = true)
+	@Query(value = "SELECT pr.user_id as id,pr.profile_name as profileName,pr.profile_image_path as profilePath,po.image_post_path as postFilePath,po.description,po.location,po.id as idea FROM user_profile as pr,user_posts as po where po.user_id=pr.user_id and pr.user_id IN (SELECT x.follwed_user_id from user_follow as x where x.user_id=:id) order by po.id desc limit 5",nativeQuery = true)
 	List<UserPost> getPostDeatils(Long id);
 	
-	@Query(value = "SELECT pr.user_id as id,pr.profile_name as profileName,pr.profile_image_path as profilePath,po.image_post_path as postFilePath,po.description,po.location,po.id as idea FROM `user_profile` as pr,`user_posts` as po where po.user_id=pr.user_id and pr.user_id=:id order by po.id desc",nativeQuery = true)
+	@Query(value = "SELECT pr.user_id as id,pr.profile_name as profileName,pr.profile_image_path as profilePath,po.image_post_path as postFilePath,po.description,po.location,po.id as idea FROM user_profile as pr,user_posts as po where po.user_id=pr.user_id and pr.user_id=:id order by po.id desc",nativeQuery = true)
 	List<UserPost> getPostDeatils1(Long id);
 }
